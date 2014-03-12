29543375e9bbeacca150c32ced3bb01e87cd5ac6
rkgpu-online <runfuture@gmail.com>
2014-03-12 16:37:16 +0800

    px2 basic version.

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -503,6 +503,10 @@ public class AdapterService extends Service {
             return service.isEnabled();
         }
 
+        public boolean isRadioEnabled() {
+            return false;
+        }
+
         public int getState() {
             // don't check caller, may be called from system UI
             AdapterService service = getService();
@@ -544,6 +548,14 @@ public class AdapterService extends Service {
             AdapterService service = getService();
             if (service == null) return false;
             return service.disable();
+        }
+
+        public boolean enableRadio() {
+            return false;
+        }
+
+        public boolean disableRadio() {
+            return false;
         }
 
         public String getAddress() {
